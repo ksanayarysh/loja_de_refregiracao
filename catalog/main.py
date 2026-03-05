42fe7195feaf3184dd38edea725012240242c997
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-03-05 11:21:20 -0300

    fix

--- catalog/main.py
+++ catalog/main.py
@@ -4,3 +4,7 @@ from router import router as catalog_router
 app = FastAPI(title="M.T.F Refrigeração — Catálogo")
 
 app.include_router(catalog_router)
+
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/static", StaticFiles(directory="catalog/static"), name="static")
