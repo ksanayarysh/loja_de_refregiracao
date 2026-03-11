9f9e180056bf67c5acaebab3ebd8d813ca53cbcb
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-03-11 10:23:19 -0300

    fix

--- catalog/router.py
+++ catalog/router.py
@@ -248,7 +248,7 @@ async def sitemap(request: Request):
         """)
 
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
         {''.join(items)}
         </urlset>
         """
