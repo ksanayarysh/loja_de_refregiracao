d905646d16a985e1e30bc977efbe62af6813ee8b
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-03-24 11:38:27 -0300

    fix

--- catalog/main.py
+++ catalog/main.py
@@ -8,3 +8,16 @@ app.include_router(catalog_router)
 from fastapi.staticfiles import StaticFiles
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+
+@app.middleware("http")
+async def force_domain(request: Request, call_next):
+    host = request.headers.get("host", "")
+
+    if "railway.app" in host:
+        url = str(request.url).replace(host, "mtfrefrigeracao.com.br")
+        return RedirectResponse(url, status_code=301)
+
+    return await call_next(request)
