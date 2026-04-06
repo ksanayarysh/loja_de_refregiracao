bd98adaa46c2a58214f391ec83850a3675cf4450
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-04-06 14:23:49 -0300

    fix

--- catalog/main.py
+++ catalog/main.py
@@ -16,22 +16,8 @@ from fastapi.responses import RedirectResponse
 async def force_domain(request: Request, call_next):
     host = request.headers.get("host", "").split(":")[0].lower()
 
-    print("HOST =", request.headers.get("host"))
-    print("URL =", request.url)
-
-    print("HOST")
-    print(host)
-
-    if host == "mtf-catalog.up.railway.app":
-        return RedirectResponse(
-            str(request.url).replace(request.headers.get("host", ""), "www.mtfrefrigeracao.com.br"),
-            status_code=301
-        )
-
-    if host == "mtfrefrigeracao.com.br":
-        return RedirectResponse(
-            str(request.url).replace(request.headers.get("host", ""), "www.mtfrefrigeracao.com.br"),
-            status_code=301
-        )
+    if host == "mtf-catalog.up.railway.app" or host == "mtfrefrigeracao.com.br":
+        new_url = str(request.url).replace(host, "www.mtfrefrigeracao.com.br", 1)
+        return RedirectResponse(new_url, status_code=301)
 
     return await call_next(request)
